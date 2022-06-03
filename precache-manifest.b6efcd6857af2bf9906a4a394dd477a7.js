dcca949a6d666dd2a4abb6764a460f842ebbf9f7
Leonardo Puchetta <rissottona@gmail.com>
2022-06-03 12:25:31 -0300

    Updates

---
+++ precache-manifest.b6efcd6857af2bf9906a4a394dd477a7.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd044f1fd4aa4ab43ecb2cc2e355382d",
+    "url": "/despliegue-app-curso-mern/index.html"
+  },
+  {
+    "revision": "37f54c72b57042a796b5",
+    "url": "/despliegue-app-curso-mern/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "92be10bf1026b829203d",
+    "url": "/despliegue-app-curso-mern/static/css/main.c194f5bf.chunk.css"
+  },
+  {
+    "revision": "37f54c72b57042a796b5",
+    "url": "/despliegue-app-curso-mern/static/js/2.18a4c505.chunk.js"
+  },
+  {
+    "revision": "542c91f071fe828f7759f188e3ae7231",
+    "url": "/despliegue-app-curso-mern/static/js/2.18a4c505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92be10bf1026b829203d",
+    "url": "/despliegue-app-curso-mern/static/js/main.95af60f3.chunk.js"
+  },
+  {
+    "revision": "cd9698f346b28fd53c2d",
+    "url": "/despliegue-app-curso-mern/static/js/runtime-main.903901bc.js"
+  },
+  {
+    "revision": "d817e8c0ed3d6d23cc0770b760e5b2c6",
+    "url": "/despliegue-app-curso-mern/static/media/Avatar1.d817e8c0.png"
+  },
+  {
+    "revision": "81f49ae883ae3f49cf35a41ecfe79b97",
+    "url": "/despliegue-app-curso-mern/static/media/Avatar2.81f49ae8.png"
+  },
+  {
+    "revision": "ffe414d448162f61e9edfa1b648c4053",
+    "url": "/despliegue-app-curso-mern/static/media/Avatar3.ffe414d4.png"
+  },
+  {
+    "revision": "286b34c5d197192938aa25f609e0181c",
+    "url": "/despliegue-app-curso-mern/static/media/Avatar4.286b34c5.png"
+  },
+  {
+    "revision": "620c02b96b28a9da53eb8b92bb50c09f",
+    "url": "/despliegue-app-curso-mern/static/media/academy-logo.620c02b9.png"
+  },
+  {
+    "revision": "c9c8c40beb27e368130687d95cfeca2a",
+    "url": "/despliegue-app-curso-mern/static/media/background-v1.c9c8c40b.jpg"
+  },
+  {
+    "revision": "822f3071338a171d4b8560b5176be436",
+    "url": "/despliegue-app-curso-mern/static/media/banner.822f3071.jpg"
+  },
+  {
+    "revision": "5d188bd45539517cbe0967ca8419f89f",
+    "url": "/despliegue-app-curso-mern/static/media/css-grid.5d188bd4.jpg"
+  },
+  {
+    "revision": "d270a2adeebd93c3c71f452e35e8ccc2",
+    "url": "/despliegue-app-curso-mern/static/media/facebook.d270a2ad.svg"
+  },
+  {
+    "revision": "03c1fcb501bf2fc07d09d950a5b1857d",
+    "url": "/despliegue-app-curso-mern/static/media/instagram.03c1fcb5.svg"
+  },
+  {
+    "revision": "26c0437b775b84db3526dd408e0d6d4c",
+    "url": "/despliegue-app-curso-mern/static/media/javascript-es6.26c0437b.jpg"
+  },
+  {
+    "revision": "c8f75fab6e40859c9e879d516e63b65c",
+    "url": "/despliegue-app-curso-mern/static/media/linkedin.c8f75fab.svg"
+  },
+  {
+    "revision": "77bcaf85be44694d82605ce9c710ef80",
+    "url": "/despliegue-app-curso-mern/static/media/logo.77bcaf85.JPG"
+  },
+  {
+    "revision": "0b68c9e06d5ccfee9e73c4f279331440",
+    "url": "/despliegue-app-curso-mern/static/media/prestashop-1-7.0b68c9e0.jpg"
+  },
+  {
+    "revision": "423a3c28b5078d080f4a5362b4588f75",
+    "url": "/despliegue-app-curso-mern/static/media/react-js-hooks.423a3c28.jpg"
+  },
+  {
+    "revision": "99cbeed95ed320ed49fa8bdfee786aed",
+    "url": "/despliegue-app-curso-mern/static/media/react-native.99cbeed9.jpg"
+  },
+  {
+    "revision": "534feb041c1a6bff0548aee325f5a0f5",
+    "url": "/despliegue-app-curso-mern/static/media/twitter.534feb04.svg"
+  },
+  {
+    "revision": "be7be3caa819d97657399b3ecf0acbe0",
+    "url": "/despliegue-app-curso-mern/static/media/wordpress.be7be3ca.jpg"
+  },
+  {
+    "revision": "d152717c5f3293517ce7bf0244a1a931",
+    "url": "/despliegue-app-curso-mern/static/media/youtube.d152717c.svg"
+  }
+]);
